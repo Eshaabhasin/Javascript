33a9cc70a581359b82c0d31e960f7d61593c0b5c
Eshaabhasin <eshaabhasin4@gmail.com>
2023-11-21 22:54:03 +0530

    added code

---
+++ Day 3/datatypes1.js
@@ -0,0 +1,26 @@
+//Primitive
+//7 types Number String Boolean Null undefined symbol BigInt
+const score=100
+const scorevalue=100.3
+
+const isloggedin=false
+const outsidetemp=null
+let useremail;
+
+const id=Symbol('123')
+const anotherid=Symbol('123')
+console.log(id===anotherid);
+
+
+const bigNumber=123456789n
+const heros=["shaktiman","spiderman"];
+let myObj={
+    name:"eshaa",
+    age:19,
+}
+const myfunction=function(){
+    console.log("Hello world");
+}
+console.log(typeof(heros));
+//Reference(Non Primitive)
+//Arrays,Objects,Functions
